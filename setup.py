b621fe283d10c0f96a5eec05c274268395f2069e
Andrea <andrea@kaist.ac.kr>
2022-08-05 17:00:55 +0900

    WIP

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name='pyblinkpico_text',
+    version='0.1.0',
+    description='The BlinkPico text library',
+    url='https://github.com/ID220/BlinkPico-Text',
+    author='Nishant Nepal & Andrea Bianchi',
+    author_email='andrea@kaist.ac.kr',
+    license='MIT',
+    packages=['pyblinkpico_text'],
+    install_requires=['pyblinkpico'],
+    keywords=['education', 'matrix_shield', 'HT16K33', 'RPI Pico'],
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: Implementation :: MicroPython'
+    ],
+)
